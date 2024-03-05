1bcb31267238bd0e839b01d3682f29813d708f2a
Gyula Zakor <gyula.zakor@htecgroup.com>
2024-03-05 09:40:53 +0000

    Update model testing script to execute in more steps instead of one big

--- workflow_scripts/run_mgx_jobs.sh
+++ workflow_scripts/run_mgx_jobs.sh
@@ -5,8 +5,11 @@ export PYTHONPATH=/opt/rocm/lib:$PYTHONPATH
 export LD_LIBRARY_PATH=/opt/rocm/lib:$LD_LIBRARY_PATH
 apt-get install git-lfs
 
-python3 -u workflow_scripts/test_models.py --all_models --target migraphx
+python3 -u workflow_scripts/test_models.py --all_models --target migraphx --quant fp32
 python3 -u workflow_scripts/mgx_verify.py --mgx-path /code/AMDMIGraphX/build/bin/migraphx-driver --input MIGRAPHX_fp32.md --output fp32_verify.txt
-python3 -u workflow_scripts/mgx_verify.py --mgx-path /code/AMDMIGraphX/build/bin/migraphx-driver --input MIGRAPHX_fp32-int8.md --output fp32_int8_verify.txt
-python3 -u workflow_scripts/mgx_verify.py --mgx-path /code/AMDMIGraphX/build/bin/migraphx-driver --input MIGRAPHX_fp32-qdq.md --output fp32_qdq_verify.txt
+python3 -u workflow_scripts/test_models.py --all_models --target migraphx --quant fp16
 python3 -u workflow_scripts/mgx_verify.py --mgx-path /code/AMDMIGraphX/build/bin/migraphx-driver --fp16 --input MIGRAPHX_fp16.md --output fp16_verify.txt
+python3 -u workflow_scripts/test_models.py --all_models --target migraphx --quant fp32-int8
+python3 -u workflow_scripts/mgx_verify.py --mgx-path /code/AMDMIGraphX/build/bin/migraphx-driver --input MIGRAPHX_fp32-int8.md --output fp32_int8_verify.txt
+python3 -u workflow_scripts/test_models.py --all_models --target migraphx --quant fp32-qdq
+python3 -u workflow_scripts/mgx_verify.py --mgx-path /code/AMDMIGraphX/build/bin/migraphx-driver --input MIGRAPHX_fp32-qdq.md --output fp32_qdq_verify.txt
